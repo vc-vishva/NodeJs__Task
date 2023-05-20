756b6dd27b64bcb711f34e520c17a68c73aa9fe9
Vishva Dadhaniya <vishva.dadhaniya@viitor.cloud>
2023-05-20 07:53:44 +0530

    pass response in json form and remove unwanted space

--- authentication-task/jwt.router.js
+++ authentication-task/jwt.router.js
@@ -1,3 +1,127 @@
+// const jwtRouter = require("express").Router();
+// const bcrypt = require("bcrypt");
+// const bodyParser = require("body-parser");
+// jwtRouter.use(bodyParser.json());
+// const jwt = require("jsonwebtoken");
+// const dbPath = process.env.DATA_PATH;
+// const fs = require("fs/promises");
+
+// jwtRouter.post("/register", async (req, res) => {
+//   try {
+//     const { name, email, password } = req.body;
+
+//     // password is not empty
+//     if (!password || password.trim() === "") {
+//       return res.status(400).send("Password is required.");
+//     }
+
+//     // hash password
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     // Read db.json file
+//     const data = await fs.readFile("db.json");
+//     const users = JSON.parse(data.toString());
+//     // Check if email already exists
+//     const user = users.find((u) => u.email === email);
+//     if (user) {
+//       // return res.status(400).send("User already registered.");
+//       return res
+//         .status(400)
+//         .json({
+//           status: 400,
+//           Message: "User already registered.",
+//           user: { name, email, password: hashedPassword },
+//         });
+//     }
+//     // Add new user
+//     users.push({ name, email, password: hashedPassword });
+//     await fs.writeFile("db.json", JSON.stringify(users));
+//     // res.send("User registered successfully.");
+//     res
+//       .status(200)
+//       .json({
+//         status: 200,
+//         Message: "User registered successfully.",
+//         user: { name, email, password: hashedPassword },
+//       });
+//   } catch (err) {
+//     res.status(500).send("Server error.");
+//   }
+// });
+
+// jwtRouter.post("/login", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if email exists in db.json file
+//     const data = await fs.readFile(dbPath);
+//     const users = JSON.parse(data.toString());
+//     const user = users.find((u) => u.email === email);
+//     if (!user) {
+//       return res.status(401).send("Invalid email or password.");
+//     }
+
+//     // Compare password with hashed password in db.json file
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(401).send("Invalid email or password.");
+//     }
+//     // Create and send JWT token
+//     const token = jwt.sign(
+//       { email: user.email, password: user.password },
+//       process.env.SECRET_KEY
+//     );
+//     res.send({ token });
+//   } catch (err) {
+//     res.status(500).send("Server error.");
+//   }
+// });
+
+// const verifyToken = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send("No token provided.");
+//   }
+
+//   const token = authHeader.split(" ")[1];
+//   try {
+//     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//     req.email = decoded.email;
+//     req.password = decoded.password;
+//     next();
+//   } catch (err) {
+//     return res.status(401).send("Invalid token.");
+//   }
+// };
+
+// // Protected API
+// jwtRouter.get("/protected", verifyToken, async (req, res) => {
+//   try {
+//     const data = await fs.readFile(dbPath);
+//     const users = JSON.parse(data.toString());
+//     const user = users.find(
+//       (u) => u.email === req.email && u.password === req.password
+//     );
+//     if (!user) {
+//       return res.status(404).send("User not found.");
+//     }
+
+//     const response = {
+//       message: "Welcome",
+//       user: {
+//         name: user.name,
+//         email: user.email,
+//       },
+//     };
+
+//     res.send(response);
+//   } catch (err) {
+//     res.status(500).send("Server error.");
+//   }
+// });
+
+// module.exports = jwtRouter;
 const jwtRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
@@ -12,7 +136,7 @@ jwtRouter.post("/register", async (req, res) => {
 
     // password is not empty
     if (!password || password.trim() === "") {
-      return res.status(400).send("Password is required.");
+      return res.status(400).json({ error: "Password is required." });
     }
 
     // hash password
@@ -25,28 +149,20 @@ jwtRouter.post("/register", async (req, res) => {
     // Check if email already exists
     const user = users.find((u) => u.email === email);
     if (user) {
-      // return res.status(400).send("User already registered.");
-      return res
-        .status(400)
-        .json({
-          status: 400,
-          Message: "User already registered.",
-          user: { name, email, password: hashedPassword },
-        });
+      return res.status(400).json({
+        error: "User already registered.",
+        user: { name, email, password: hashedPassword },
+      });
     }
     // Add new user
     users.push({ name, email, password: hashedPassword });
     await fs.writeFile("db.json", JSON.stringify(users));
-    // res.send("User registered successfully.");
-    res
-      .status(200)
-      .json({
-        status: 200,
-        Message: "User registered successfully.",
-        user: { name, email, password: hashedPassword },
-      });
+    res.status(200).json({
+      message: "User registered successfully.",
+      user: { name, email, password: hashedPassword },
+    });
   } catch (err) {
-    res.status(500).send("Server error.");
+    res.status(500).json({ error: "Server error." });
   }
 });
 
@@ -59,29 +175,29 @@ jwtRouter.post("/login", async (req, res) => {
     const users = JSON.parse(data.toString());
     const user = users.find((u) => u.email === email);
     if (!user) {
-      return res.status(401).send("Invalid email or password.");
+      return res.status(401).json({ error: "Invalid email or password." });
     }
 
     // Compare password with hashed password in db.json file
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).send("Invalid email or password.");
+      return res.status(401).json({ error: "Invalid email or password." });
     }
     // Create and send JWT token
     const token = jwt.sign(
       { email: user.email, password: user.password },
       process.env.SECRET_KEY
     );
-    res.send({ token });
+    res.json({ token });
   } catch (err) {
-    res.status(500).send("Server error.");
+    res.status(500).json({ error: "Server error." });
   }
 });
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send("No token provided.");
+    return res.status(401).json({ error: "No token provided." });
   }
 
   const token = authHeader.split(" ")[1];
@@ -91,7 +207,7 @@ const verifyToken = (req, res, next) => {
     req.password = decoded.password;
     next();
   } catch (err) {
-    return res.status(401).send("Invalid token.");
+    return res.status(401).json({ error: "Invalid token." });
   }
 };
 
@@ -104,7 +220,7 @@ jwtRouter.get("/protected", verifyToken, async (req, res) => {
       (u) => u.email === req.email && u.password === req.password
     );
     if (!user) {
-      return res.status(404).send("User not found.");
+      return res.status(404).json({ error: "User not found." });
     }
 
     const response = {
@@ -115,10 +231,9 @@ jwtRouter.get("/protected", verifyToken, async (req, res) => {
       },
     };
 
-    res.send(response);
+    res.json(response);
   } catch (err) {
-    res.status(500).send("Server error.");
+    res.status(500).json({ error: "Server error." });
   }
 });
-
 module.exports = jwtRouter;
