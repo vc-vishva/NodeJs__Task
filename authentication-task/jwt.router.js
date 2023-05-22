8241a0ccf9ec87f3a0ebc1d9925f6bbb797ceee4
Vishva Dadhaniya <vishva.dadhaniya@viitor.cloud>
2023-05-22 05:51:44 +0530

    done all changes

--- authentication-task/jwt.router.js
+++ authentication-task/jwt.router.js
@@ -10,33 +10,26 @@ const fs = require("fs/promises");
 jwtRouter.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
     // password is not empty
     if (!password || password.trim() === "") {
       return res.status(400).json({ error: "Password is required." });
     }
-
-    // hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
     // Read db.json file
     const data = await fs.readFile("db.json");
     const users = JSON.parse(data.toString());
     // Check if email already exists
     const user = users.find((u) => u.email === email);
     if (user) {
-      return res.status(400).json({
-        error: "User already registered.",
-        user: { name, email, password: hashedPassword },
-      });
+      return res.status(400).json({ error: "User already registered." });
     }
     // Add new user
     users.push({ name, email, password: hashedPassword });
     await fs.writeFile("db.json", JSON.stringify(users));
     res.status(200).json({
       message: "User registered successfully.",
-      user: { name, email, password: hashedPassword },
+      user: { name, email },
     });
   } catch (err) {
     res.status(500).json({ error: "Server error." });
@@ -47,7 +40,6 @@ jwtRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if email exists in db.json file
     const data = await fs.readFile(dbPath);
     const users = JSON.parse(data.toString());
     const user = users.find((u) => u.email === email);
@@ -55,17 +47,16 @@ jwtRouter.post("/login", async (req, res) => {
       return res.status(401).json({ error: "Invalid email or password." });
     }
 
-    // Compare password with hashed password in db.json file
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ error: "Invalid email or password." });
     }
-    // Create and send JWT token
+
     const token = jwt.sign(
       { email: user.email, password: user.password },
       process.env.SECRET_KEY
     );
-    res.json({ token });
+    res.json({ token: token });
   } catch (err) {
     res.status(500).json({ error: "Server error." });
   }
@@ -76,7 +67,6 @@ const verifyToken = (req, res, next) => {
   if (!authHeader) {
     return res.status(401).json({ error: "No token provided." });
   }
-
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
@@ -98,16 +88,12 @@ jwtRouter.get("/protected", verifyToken, async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found." });
     }
-
-    const response = {
-      message: "Welcome",
+    res.json({
       user: {
         name: user.name,
         email: user.email,
       },
-    };
-
-    res.json(response);
+    });
   } catch (err) {
     res.status(500).json({ error: "Server error." });
   }
