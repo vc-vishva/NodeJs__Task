0cc5ac33af99e3c0c5f43ff6adb7585f1e3d0a9d
vishva dadhaniya <vishva.dadhaniya@viitor.cloud>
2023-07-10 20:49:46 +0530

    correct validation

--- tip-management-TS/src/validation/tipValidation.ts
+++ tip-management-TS/src/validation/tipValidation.ts
@@ -4,18 +4,18 @@ export const validatePlace = [
   body("placeName")
     .notEmpty()
     .withMessage("Place name is required")
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .isString(),
   body("billAmount")
     .notEmpty()
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .withMessage("Bill amount is required")
     .isNumeric()
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .withMessage("Bill amount must be a number"),
   body("tipAmount")
     .notEmpty()
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .withMessage("Tip amount is required")
     .isNumeric()
     .withMessage("Tip amount must be a number"),
--- tip-management-TS/src/validation/userValidation.ts
+++ tip-management-TS/src/validation/userValidation.ts
@@ -4,23 +4,23 @@ export const validateUser = [
   body("f_name")
     .notEmpty()
     .withMessage("First name is required")
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .isString(),
   body("l_name")
     .notEmpty()
     .withMessage("Last name is required")
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .isString(),
   body("email")
     .notEmpty()
     .withMessage("Email is required")
     .isEmail()
     .withMessage("Invalid email address")
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .isString(),
   body("password")
     .notEmpty()
     .withMessage("Password is required")
-    .isLength({ min: 25 })
+    .isLength({ max: 25 })
     .isString(),
 ];
